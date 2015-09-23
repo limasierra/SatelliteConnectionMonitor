d28b335bf3db3a7a5cac58b17fcbdfc952cd991f
Laurent Seiler <laurent.seiler@student.uibk.ac.at>
2015-09-23 11:57:24 +0200

    Downloaded remote web interface libraries

--- web_interface/index.php
+++ web_interface/index.php
@@ -21,8 +21,8 @@
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
     <![endif]-->
   </head>
 
@@ -76,9 +76,9 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js" charset="utf-8"></script>
+    <script src="js/d3.min.js" charset="utf-8"></script>
     <script src="js/nv.d3.js" charset="utf-8"></script>
     <script src="js/stream_layers.js"></script>
 
